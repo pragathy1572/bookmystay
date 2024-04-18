c4c70c4d3c905303b1731fa8a689a4d9d3d70400
unknown <pragathy1572@gmail.com>
2024-04-18 18:48:53 +0530

    added my hotels view and e2e tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,35 +10,71 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 
 const App = () => {
   const {isLoggedIn} = useAppContext();
   return (
     <Router>
       <Routes>
-        <Route path='/' element=
-        {
-          <Layout>
-            <p>Home Page</p>
-          </Layout>
-        }>
-        </Route>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        ></Route>
 
-        <Route path='search' element=
-        {
-          <Layout>
-            <p>Search Page</p>
-          </Layout>
-        }>
-        </Route>
+        <Route
+          path="search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        ></Route>
 
-        <Route path="/register" element={<Layout><Register /></Layout>}></Route>
-        <Route path="/sign-in" element={<Layout><SignIn /></Layout>}></Route>
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        ></Route>
+        <Route
+          path="/sign-in"
+          element={
+            <Layout>
+              <SignIn />
+            </Layout>
+          }
+        ></Route>
 
-        {isLoggedIn && <>
-        <Route path="/add-hotel" element={<Layout><AddHotel /></Layout>}></Route></>}
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            ></Route>
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            ></Route>
+          </>
+          
+        )}
 
-        <Route path='*' element={<Navigate to="/" />}></Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
       </Routes>
     </Router>
   );
