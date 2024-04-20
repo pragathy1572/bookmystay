407276f204539441c2b2701db5a9d0e383b69cda
unknown <pragathy1572@gmail.com>
2024-04-20 15:53:45 +0530

    added edit hotel feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
 
 const App = () => {
   const {isLoggedIn} = useAppContext();
@@ -67,6 +68,14 @@ const App = () => {
               element={
                 <Layout>
                   <MyHotels />
+                </Layout>
+              }
+            ></Route>
+            <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel />
                 </Layout>
               }
             ></Route>
--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -88,3 +88,29 @@ export const fetchMyHotels = async (): Promise<HotelType[]> => {
 
     return response.json();
 }
+
+export const fetchMyHotelById = async (hotelId: string): Promise<HotelType[]> => {
+    const response = await fetch(`${API_BASE_URL}/api/my-hotels/${hotelId}`, {
+        credentials: "include",
+    });
+
+    if(!response.ok) {
+        throw new Error("Error fetching hotel");
+    }
+
+    return response.json();
+}
+
+export const updatedHotelById = async (hotelformData: FormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/my-hotels/${hotelformData.get("hotelId")}`, {
+        method: "PUT",
+        body: hotelformData,
+        credentials: "include",
+    });
+
+    if(!response.ok) {
+        throw new Error("Failed to update Hotel");
+    }
+
+    return response.json();
+}
